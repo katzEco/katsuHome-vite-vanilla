68b5bdb9f3d8f07a1c140020ec009a21f869d80b
Suphakit P <me@suphakit.net>
2023-01-25 01:29:09 +0700

    feat: setting up tailwind

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ import './style.css'
 
 document.querySelector('#app').innerHTML = `
   <main>
-
+    <div class="w-[100%] h-screen flex flex-col justify-center text-center items-center">
+      <div class="card w-[20rem] bg-[#f5f5f5] p-[1rem] rounded-[1rem]"></div>
+    </div>
   </main>
 `
