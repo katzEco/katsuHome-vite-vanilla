d4125ea68707b9b8758682ca6c0b98196dbee085
Suphakit P <me@suphakit.net>
2023-02-08 10:51:25 +0000

    feat: set up config

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+const { resolve } = require('path')
+
+module.exports = {
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/page/about/index.html'),
+        commission: resolve(__dirname, 'src/page/commission/index.html'),
+        home: resolve(__dirname, 'src/page/home/index.html'),
+        donation: resolve(__dirname, 'src/page/donation/index.html'),
+      }
+    }
+  }
+}
