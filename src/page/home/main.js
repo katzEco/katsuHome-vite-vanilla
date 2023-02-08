619971989ce5d00f654009affdc76b13a782f301
Suphakit P <me@suphakit.net>
2023-02-08 10:49:41 +0000

    feat: add page

---
+++ src/page/home/main.js
@@ -0,0 +1,25 @@
+import './src/css/tailwind.css'
+import './style.css'
+import { frame, page } from './src/js/compo'
+
+function pageFrame(inp) {
+  let xConst = `
+    ${frame().nav}
+
+    ${page(inp)}
+    
+    ${frame().footer}
+  `
+
+  return xConst
+}
+
+let bURL = window.location.href.split('/')
+bURL.pop()
+bURL = bURL.join('/')
+let pLocation = window.location.href.replace(bURL + '/', '')
+console.log(pLocation);
+
+document.querySelector('#app').innerHTML = `
+  ${pageFrame(pLocation)}
+`
